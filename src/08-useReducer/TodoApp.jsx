8fc32a1b1d806813fd91398b7ec775fc3e38a370
AYGJ <jofre1236@gmail.com>
2022-10-02 12:27:20 -0500

    Change with of todoApp

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -12,7 +12,7 @@ const TodoApp = () => {
   } = useTodos();
 
   return (
-    <main className="container mx-auto mt-5 w-11/12 md:w-full">
+    <main className="container mx-auto mt-5 w-11/12">
       <h1 className="text-4xl text-center text-white font-bold">Todo APP</h1>
       <p className="text-center text-gray-300">Todos {todosCount()}</p>
       <p className="text-center text-gray-300">
